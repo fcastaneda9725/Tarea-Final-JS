733c8d425ba03ac200a0f9b81141b71ed5e0813a
fcastaneda9725 <f.castanedat@gmail.com>
2018-03-29 13:01:57 -0600

    Declaración de las primeras variables a utilizar

---
+++ js/app.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  calculadora();
+};
+
+function calculadora() {
+  //Declaración de la variables a utilizar
+  var pantalla = document.getElementById('display');
+  var on = document.getElementById('on');
+  var signo = document.getElementById('sign');
+  var raiz = document.getElementById('raiz');
+  var division = document.getElementById('dividido');
+  var multiplicacion = document.getElementById('por');
+  var resta = document.getElementById('menos');
+  var suma = document.getElementById('mas');
+  var igual = document.getElementById('igual');
+  var punto = document.getElementById('punto');
+  var cero = document.getElementById('0');
+  var uno = document.getElementById('1');
+  var dos = document.getElementById('2');
+  var tres = document.getElementById('3');
+  var cuatro = document.getElementById('4');
+  var cinco = document.getElementById('5');
+  var seis = document.getElementById('6');
+  var siete = document.getElementById('7');
+  var ocho = document.getElementById('8');
+  var nueve = document.getElementById('9');
+
+}
